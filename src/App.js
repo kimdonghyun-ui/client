718dacda384d4e204e1afa492778413093c3b47e
dongdong <dongdong@eyesvision.com>
2021-06-10 17:58:20 +0900

    200제-39p 할차례

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import LifecycleEx from "./R008_LifecycleEx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Start React 200!</h1>
+      <p>css 적용하기</p>
+      <LifecycleEx prop_value="FromApp.js" />
     </div>
   );
 }

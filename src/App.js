234b1d5c23140e304121d68f15d8577d393503d5
dongdong <dongdong@eyesvision.com>
2021-06-11 18:02:33 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
-import LifecycleEx from "./R008_LifecycleEx";
+import PropsDatatype from "./R018_PropsDatatype";
 
 function App() {
   return (
     <div>
-      <h1>Start React 200!</h1>
-      <p>css 적용하기</p>
-      <LifecycleEx prop_value="FromApp.js" />
+      {/* <h1>Start React 200!</h1>
+      <p>css 적용하기</p> */}
+      <PropsDatatype
+        String="react"
+        Number={200}
+        Boolean={1==2}
+        Array={[0, 1, 8]}
+        ObjectJson={{react:"리액트", twohundred:"200"}}
+        Function={console.log("FunctionProps: function!")}
+      />
     </div>
   );
 }
